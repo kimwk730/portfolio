519585c64e10ab0c5a8a6376d8cd2681156c2db2
kimwk730 <kimwk730@users.noreply.github.com>
2023-09-13 01:19:17 -1000

    Footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+	return (
+		<div className="footer">
+			<div className="socialMedia">icons</div>
+			<p> &copy; 2023 wonkim.dev</p>
+		</div>
+	);
+}
+
+export default Footer;
