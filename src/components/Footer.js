397bbaa4c183417bbdfbda94c259194aa125915c
kimwk730 <kimwk730@users.noreply.github.com>
2023-09-20 02:02:16 -1000

    work inquiry added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 import "../styles/Footer.css";
 
 function Footer() {
 	return (
 		<div className="footer">
-			<div className="socialMedia">icons</div>
-			<p> &copy; 2023 wonkim.dev</p>
+			<div className="inquiryBox">
+				<div>
+					<h2>Work Inquiry</h2>
+					<h3>Let's work together</h3>
+				</div>
+				<button>Contact me</button>
+			</div>
+			<div className="linkIcons">
+				<FontAwesomeIcon icon={faGithub} className="link" title="Github" />
+				<FontAwesomeIcon icon={faLinkedinIn} />
+				<FontAwesomeIcon icon={faEnvelope} />
+			</div>
+
+			<p>
+				<a href="../pages/Contact.js">Open source</a> website coded by Won Kim{" "}
+				<br />
+				&copy; 2023 wonkim.dev
+			</p>
 		</div>
 	);
 }
