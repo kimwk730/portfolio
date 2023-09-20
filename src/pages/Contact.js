192df8126dc4b0b89b6b4aeaaa0e73726564459e
kimwk730 <kimwk730@users.noreply.github.com>
2023-09-20 01:16:34 -1000

    contact page created

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,13 +2,19 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faMobile } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Contact.css";
 
 function Contact() {
 	return (
-		<div>
-			Contact
-			<FontAwesomeIcon icon={faEnvelope} />
-			<FontAwesomeIcon icon={faMobile} />
+		<div className="contact">
+			<h1>Let's keep in touch!</h1>
+			<h2>
+				<FontAwesomeIcon icon={faEnvelope} /> kimwk730@gmail.com
+			</h2>
+
+			<h2>
+				<FontAwesomeIcon icon={faMobile} /> 808-397-8852
+			</h2>
 		</div>
 	);
 }
